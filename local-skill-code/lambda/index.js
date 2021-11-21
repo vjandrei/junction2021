b2ce590520b0e8bc1a29f24eb951952fa64fd69e
Andreas Koutsoukos <andreas.koutsoukos@gmail.com>
2021-11-21 08:17:02 +0200

    added local code

---
+++ local-skill-code/lambda/index.js
@@ -0,0 +1,245 @@
+/* *
+ * This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
+ * Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
+ * session persistence, api calls, and more.
+ * */
+const Alexa = require('ask-sdk-core');
+
+const PAUSE = '<break time="0.25s"/>';
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const AlexaInvocationOutputStart = 'Welcome to the house information app. How may I help you?';
+        
+        return handlerInput.responseBuilder
+            .speak(AlexaInvocationOutputStart)
+            .reprompt(AlexaInvocationOutputStart)
+            .getResponse();
+    }
+};
+
+
+const ORASHouseInformationIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ORASHouseInformationIntent';
+    },
+    handle(handlerInput) {
+        const EnergyConsumptionSelector = Alexa.getSlotValue(handlerInput.requestEnvelope, 'EnergyConsumption');
+        const EnergyConsumptionOutputEnd = EnergyConsumptionSelector ? `I will get the 
+        ${EnergyConsumptionSelector} consumption data for you. Please wait.` 
+        : `Sorry cant get the ${EnergyConsumptionSelector} data right now.`
+      
+        return handlerInput.responseBuilder
+            .speak(EnergyConsumptionSelector)
+            .reprompt(EnergyConsumptionSelector)
+            .speak(EnergyConsumptionOutputEnd)
+            .getResponse();
+    }
+};
+
+
+const ORASWaterIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ORASWaterIntent';
+    },
+    async handle(handlerInput) {
+        const waterConsumptionOutputStart = `I have now your ORAS water consumption data ready now. 
+        Which of these options you want to select? Dishwasher, Washing machine, Toilet, Kitchen or Shower`
+        
+        return handlerInput.responseBuilder
+        .speak(waterConsumptionOutputStart)
+        .reprompt(waterConsumptionOutputStart)
+        .getResponse();
+    }
+}
+
+
+
+const ORASShowerIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ORASShowerIntent';
+    },
+    handle(handlerInput) {
+        
+        const showerDataOutputStart = 
+        `According to data, you consumed a little warm water in the shower last week which is great. ${PAUSE}
+        You have saved 32 liters of water which is about a CO2 emission factor of 600 kg CO2 / MWh. ${PAUSE}
+        Good job. ${PAUSE}
+        On the other hand, you have not adhered to hygiene. According to Stara’s sports data, you had 
+        been playing sports for two days last week, but you didn’t take a shower. ${PAUSE} You nasty go now to shower! ${PAUSE}
+        Was there anything else you liked to know about the shower data?
+        `;
+        return handlerInput.responseBuilder
+            .speak(showerDataOutputStart)
+            .reprompt(showerDataOutputStart)
+            .getResponse();
+    }
+};
+
+
+
+
+const ORASShowerUsageIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'ORASShowerUsageIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = `Let me check the ORAS Hydractiva shower data. ${PAUSE} 
+        According to machine learning shower model you like long hot showers. ${PAUSE} I would recommend you to enable the shower routine help function. ${PAUSE} 
+        Do you want to enable this feature?`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+
+const HelloWorldIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Hello World!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        ORASHouseInformationIntentHandler,
+        ORASWaterIntentHandler,
+        ORASShowerIntentHandler,
+        ORASShowerUsageIntentHandler,
+        HelloWorldIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
